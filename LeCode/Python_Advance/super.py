4fac9fdf359357957a902eb34020a10f320233f3
Hairui Gao <73648811+GodDragonStr@users.noreply.github.com>
2023-07-22 17:59:09 +0800

    7/22

--- LeCode/Python_Advance/super.py
+++ LeCode/Python_Advance/super.py
@@ -11,4 +11,27 @@ class Male(Person):
         Person.__init__(self, name)
         self.gender = "male"
 
+class A:
+    def say(self):
+        print("A")
+
+class B(A):
+    def say(self):
+        super().say()
+
+class D(A):
+    def say(self):
+        print("D")
+
+class C(B):
+    def say(self):
+        super().say()
+
+class M(B, D):
+    def say(self):
+        # super().say()
+        B.say(self)
+
+my_object = M()
+my_object.say()
 
