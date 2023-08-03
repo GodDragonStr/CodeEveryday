91705082468e7085eaa55eb6dfb9588742aa00b9
Hairui Gao <73648811+GodDragonStr@users.noreply.github.com>
2023-08-03 09:43:03 +0800

    8/3

---
+++ LeCode/Pic_Data_Structure/pre_stack.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class my_stack {
+/* 实现入栈push，出栈pop，堆栈是否为空isEmpty，堆栈是否满isFull
+ * 在实现时需要输出一条标志语句
+ * 写一个main函数直接调用这些成员函数
+*/
+
+public:
+
+
+private:
+
+
+}
+
+
+int main () {
+
+    return 0;
+}
